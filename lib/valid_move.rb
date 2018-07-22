2935a8b4c05b5e41fa426b722c546c1ecd39d48c
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-22 13:28:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
-
+def valid_move?()
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  # if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+  # below makes use of short-circuit nature of evaluation in Ruby
+  if (!board[index]) || (board[index].strip == "") 
+    false
+  else
+    true
+  end
+end
